ff29f3595be2ee8c1e8586d8f1a2c697a06125fb
PeerJack22 <pajocina.11@gmail.com>
2026-04-29 11:27:21 -0500

    Icono y splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
+/// <reference types="@capacitor/splash-screen" />
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'pokeapp',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+      launchShowDuration: 0,
+      backgroundColor: '#ffffff',
+    },
+  },
 };
 
 export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => SplashScreen.hide().catch(() => undefined))
   .catch(err => console.log(err));
